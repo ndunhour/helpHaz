8c9f845b5b15c2eb8428e25232a73e54b00c65f1
ndunhourowens <ndunhour@hotmail.com>
2017-10-04 21:16:12 -1000

    changed code from slicing circles on click to change color fill's opacity

--- helpHaz.js
+++ helpHaz.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
     var c = document.getElementById("imgBox");
+    var start = document.getElementById("startBtn");
     var rect = c.getBoundingClientRect();
     var ctx = c.getContext("2d");
 
     // ball
     var ballRadius = 20;
-    var circles = [  {x:100,y:100,r:20,vx:1,vy:1}, // orange ball
-                     {x:275,y:300,r:20,vx:2,vy:-1}, // green ball
-                     {x:20,y:350,r:20,vx:-1,vy:-2}, // purple
-                     {x:300,y:70,r:20,vx:-2,vy:1}, // pink ball
-                     {x:450,y:50,r:20,vx:-1,vy:-2}  // tourquoise
+    var circles = [  {x:100,y:100,r:20,vx:1,vy:1,color:"black"},
+                     {x:275,y:300,r:20,vx:2,vy:-1,color:"black"},
+                     {x:20,y:350,r:20,vx:-1,vy:-2,color:"black"},
+                     {x:300,y:70,r:20,vx:-2,vy:1,color:"black"},
+                     {x:450,y:50,r:20,vx:-1,vy:-2,color:"black"}
                     ];
 
 
@@ -19,13 +20,14 @@ $(document).ready(function(){
     var blockH = 175;
     var blockW = 500;
 
-
-    var numOfFlies = 3;
-    var level = 1;
-    var squashedEm = 1;
+    var level = 0;
+    var squashedEm = 0;
     var touched = 5;
 
+
     c.addEventListener('click', squashed);
+    start.addEventListener('click', draw);
+
     function drawHearts(){
         for(var t = 0; t < touched; t++){
             $('#lives').append('<span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span>');
@@ -35,8 +37,9 @@ $(document).ready(function(){
     function drawBall() {
 
         for(var i=0; i <circles.length; i++){
+
              ctx.beginPath();
-             ctx.fillStyle = "black";
+             ctx.fillStyle = circles[i].color;
              ctx.arc(circles[i].x,circles[i].y,circles[i].r,0,2*Math.PI,false);
              ctx.fill();
 
@@ -68,50 +71,55 @@ $(document).ready(function(){
 
     function collisionOfBoundry(){
         for(var j=0; j<circles.length; j++){
-
             if(circles[j].x === blockX || circles[j].x === blockX + blockY || circles[j].y === blockY || circles[j].y === blockY + blockH){
-                touched -= 1;
-                $('#lives span').remove();
-                drawHearts();
+                // touched -= 1;
+                // $('#lives span').remove();
+                // drawHearts();
                 if(touched === 0){
                     $('#lives').text('GAME OVER');
                 }
+
             }
-
         }
-
     }
 
 
 
     // function levelUp(){
-    //     level += 1;
-    //     numOfFlies += 5;
-    //     dx += 1;
-    //     dx += 1;
-    //     $('#level').text(level);
+    //         level += 1;
+    //         console.log('level', level);
+
     // }
 
+    // get rid of circles
     function squashed(){
         var evtX = event.layerX;
         var evtY = event.layerY;
         for(var s=circles.length - 1; s >= 0; --s){
             if( circles[s].x - (circles[s].r + 40) < evtX && circles[s].y - (circles[s].r + 40) < evtY && circles[s].x + (circles[s].r + 40) > evtX && circles[s].y + (circles[s].r + 40) > evtY ){
-                circles.splice(s,1);
+
+                circles[s].color = "rgba(255, 255, 255, 0.01)";
+                squashedEm  += 1;
             }
+            if(circles.length === squashedEm){
+                $('#lives').text('LEVEL UP');
+                for(var redraw = 0; redraw < circles.length; redraw++){
+                    circles[redraw].color = "black";
+                    squashedEm = 0;
+            }
+        }
         }
         ctx.clearRect(0,0,c.width, c.height);
 
-        // squashedEm += 1;
-        // var remainder = numOfFlies - squashedEm;
-        // $('#squashed').text(remainder);
-
     }
 
+    // randomly position the circles
     function getRandomArbitrary(min, max) {
         return Math.random() * (max - min) + min;
     }
 
+
+    // creates canvas, balls, boundry, and collision of boundry
     function draw() {
         ctx.clearRect(0, 0, c.width, c.height);
         drawBall();
@@ -119,7 +127,6 @@ $(document).ready(function(){
         collisionOfBoundry();
     }
 
-    setInterval(draw, 10);
-
+    setInterval(draw, 20);
 
 });
